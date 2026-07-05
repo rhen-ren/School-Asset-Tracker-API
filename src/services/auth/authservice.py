ac4966aec4b7fc4fc67b66ead91e0f89c14195e7
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-05 19:17:40 +0800

    Enhance user authentication logic and add password verification

--- src/services/auth/authservice.py
+++ src/services/auth/authservice.py
@@ -1,17 +1,33 @@
 import os
 from dotenv import load_dotenv
 import jwt
+from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from src.models.user import User
+from fastapi.exceptions import HTTPException
 
 #get secret key from env
 load_dotenv()
 SECRETKEY = os.getenv("SECRETKEY")
 ALGORITHM = "HS256"
+password_hash = PasswordHash.recommended()
 
+#authenticate user
+#used for getting correct user
+#user check hashed password on db
+def authenticate_user(email: str, password: str, db: Session) -> User:
+    user: User = db.execute(select(User).where(User.email == email)).scalars().one_or_none()
+    if not user:
+        raise HTTPException(status_code=401, detail="User Not Found")
+    if not password_hash.verify(password, user.password):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return user
 
 
 #access token creation
 #uses jwt
-def create_access_token(data: dict):
+def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     encoded_jwt = jwt.encode(to_encode, SECRETKEY, algorithm=ALGORITHM)
     return encoded_jwt
