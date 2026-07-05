fbac4c6a3148fb836c84e8788ebbc81b35f1ab62
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-05 18:48:02 +0800

    Refactor authservice.py to enhance access token creation logic

--- src/services/auth/authservice.py
+++ src/services/auth/authservice.py
@@ -1,6 +1,19 @@
 import os
 from dotenv import load_dotenv
+import jwt
 
+#get secret key from env
 load_dotenv()
 SECRETKEY = os.getenv("SECRETKEY")
 ALGORITHM = "HS256"
+
+
+
+#access token creation
+#uses jwt
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    encoded_jwt = jwt.encode(to_encode, SECRETKEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
