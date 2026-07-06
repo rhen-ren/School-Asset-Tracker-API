2035ef15f8d8936a6c0615a2062f4facc7f49b53
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-06 09:47:59 +0800

    Implement user login endpoint with authentication logic

--- src/routers/userrouter.py
+++ src/routers/userrouter.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends
 from sqlalchemy.orm import Session
 from src.dependency import get_db
+from src.services import userservice
+from src.schemas.user import LoginUser
+from src.schemas.auth.token import Token
 
 router = APIRouter()
 
@@ -17,5 +20,5 @@ async def get_user(db: Session = Depends(get_db)):
     pass
 
 @router.get("/user/login")
-async def login_user(db: Session = Depends(get_db)):
-    pass
+async def login_user(user_data: LoginUser = Body(...), db: Session = Depends(get_db)) -> Token:
+    return userservice.login_user(user_data.email, user_data.password)
