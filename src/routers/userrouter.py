be99af4165e10cf77f419921734bdb64ba66cc0e
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-06 11:26:31 +0800

    Refactor user login endpoint to use OAuth2PasswordRequestForm and add signup endpoint

--- src/routers/userrouter.py
+++ src/routers/userrouter.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Body, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from src.dependency import get_db
 from src.services import userservice
-from src.schemas.user import LoginUser
+from src.schemas.user import LoginUser, SignupUser
 from src.schemas.auth.token import Token
 
 router = APIRouter()
@@ -20,5 +21,16 @@ async def get_user(db: Session = Depends(get_db)):
     pass
 
 @router.get("/user/login")
-async def login_user(user_data: LoginUser = Body(...), db: Session = Depends(get_db)) -> Token:
-    return userservice.login_user(user_data.email, user_data.password)
+async def login_user(user_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)) -> Token:
+    login_user = LoginUser(email = user_data.username, password = user_data.password)
+    return userservice.login_user(login_user.email, login_user.password)
+
+@router.post("/user/signup")
+async def signup_user(user_data: SignupUser, db: Session = Depends(get_db)) -> Token:
+    return userservice.signup_user(
+        name = user_data.name,
+        email = user_data.email,
+        password = user_data.password,
+        role = user_data.role,
+        db = db
+    )
