78d5414419d89e61ec5bf3cb1e166e8bea1273d9
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-06 09:27:49 +0800

    Refactor create_access_token to return Token object and enhance validate_token function for improved user retrieval

--- src/services/auth/authservice.py
+++ src/services/auth/authservice.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from src.models.user import User
 from fastapi.exceptions import HTTPException
+from src.schemas.auth.token import TokenData, Token
 
 #get secret key from env
 load_dotenv()
@@ -24,12 +25,28 @@ def authenticate_user(email: str, password: str, db: Session) -> User:
         raise HTTPException(status_code=401, detail="Unauthorized")
     return user
 
-
 #access token creation
 #uses jwt
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    encoded_jwt = jwt.encode(to_encode, SECRETKEY, algorithm=ALGORITHM)
-    return encoded_jwt
+def create_access_token(data: dict) -> dict:
+    to_encode: dict = data.copy()
+    encoded_jwt: str = jwt.encode(to_encode, SECRETKEY, algorithm=ALGORITHM)
+    return Token(access_token=encoded_jwt, token_type="bearer")
+
+#token validation
+#returns user account
+def validate_token(token: str, db: Session):
+    try:
+        payload: dict = jwt.decode(token, SECRETKEY, algorithms=[ALGORITHM])
+        email: str | None  = payload.get("email")
+        if email is None:
+            return None
+        token_data = TokenData(email=email)
+        user = db.execute(select(User).where(User.email == token_data.email)).scalars().one_or_none()
+        if not user:
+            raise HTTPException(status_code=401)
+        return user
+    except Exception as e:
+        raise e
+
 
 
