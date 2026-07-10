9fa19b5f980365932ef700c08f5882bae143f9ae
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-10 18:08:50 +0800

    Add create_asset endpoint with token validation and request body schema

--- src/routers/assetrouter.py
+++ src/routers/assetrouter.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
-from src.dependency import get_db
+from src.dependency import get_db, oath2_scheme
+from src.schemas.asset import CreateAsset, GetAsset
+from src.services.auth import authservice
+from src.services import assetservice
 
 router = APIRouter()
 
@@ -13,8 +16,11 @@ def get_asset(asset_id: int, db: Session = Depends(get_db)):
     pass
 
 @router.post("/asset")
-def create_asset(db: Session = Depends(get_db)):
-    pass
+def create_asset(token: str = Depends(oath2_scheme), data: CreateAsset = Body(...), db: Session = Depends(get_db)):
+    user = authservice.validate_token(token)
+    if user:
+        #TODO:
+        asset_obj = assetservice.create_asset()
 
 @router.put("/asset/{asset_id}")
 def update_asset(asset_id: int, db: Session = Depends(get_db)):
