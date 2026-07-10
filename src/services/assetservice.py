91cd980c13287861abd107278e57240ba63ee194
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-10 17:43:26 +0800

    Refactor asset service to include error handling for asset retrieval

--- src/services/assetservice.py
+++ src/services/assetservice.py
@@ -1,7 +1,16 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from src.models import Asset
+from fastapi.exceptions import HTTPException
 
 def get_assets(db: Session) -> list[Asset]:
     assets = db.execute(select(Asset)).scalars().all()
     return assets
+
+def get_asset(asset_id: int, db: Session) -> Asset:
+    asset = db.get(Asset, asset_id)
+    if asset:
+        return asset
+    else:
+        raise HTTPException(status_code=404)
+    
