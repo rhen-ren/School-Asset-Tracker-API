fe53526d7fb55df3d11d9ced81f7fe639dde0a32
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-11 10:59:47 +0800

    Implement token validation and response formatting for get_assets and get_asset endpoints

--- src/routers/assetrouter.py
+++ src/routers/assetrouter.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
 from src.dependency import get_db, oath2_scheme
+from src.models.asset import Asset
 from src.schemas.asset import CreateAsset, GetAsset
 from src.services.auth import authservice
 from src.services import assetservice
@@ -8,13 +9,33 @@ from src.services import assetservice
 router = APIRouter()
 
 @router.get("/assets")
-def get_assets(db: Session = Depends(get_db)):
-    pass
+def get_assets(token: str = Depends(oath2_scheme), db: Session = Depends(get_db)) -> list[GetAsset]:
+    user = authservice.validate_token(token)
+    if user:
+        asset_obj: list[Asset] = assetservice.get_assets(db)
+        assets: list[GetAsset] = [GetAsset(
+            id=a.id, name=a.name, status=a.status, serial_number=a.serial_number, purchase_date= a.purchase_date,
+            img_url=a.img_url, created_at=a.created_at, updated_at=a.updated_at
+        ) for a in asset_obj]
+
+        return assets
 
 @router.get("/asset/{asset_id}")
-def get_asset(asset_id: int, db: Session = Depends(get_db)):
-    pass
+def get_asset(asset_id: int, token: str = Depends(oath2_scheme), db: Session = Depends(get_db)) -> GetAsset:
+    user = authservice.validate_token(token)
+    if user:
+        asset_obj = assetservice.get_asset(asset_id, db)
 
+        return GetAsset(
+            id=asset_obj.id,
+            name=asset_obj.name,
+            status=asset_obj.status,
+            serial_number=asset_obj.serial_number,
+            purchase_date=asset_obj.purchase_date,
+            img_url=asset_obj.img_url,
+            created_at=asset_obj.created_at,
+            updated_at=asset_obj.updated_at
+        )
 @router.post("/asset")
 def create_asset(token: str = Depends(oath2_scheme), data: CreateAsset = Body(...), db: Session = Depends(get_db)):
     user = authservice.validate_token(token)
