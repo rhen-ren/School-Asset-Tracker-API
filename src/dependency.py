fc912b9699bf74052d2f4457ab1c934f00dabd78
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-12 00:10:31 +0800

    Add image upload functionality with save_img method

--- src/dependency.py
+++ src/dependency.py
@@ -1,5 +1,7 @@
+import shutil
 from src.db import SessionLocal
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import File, UploadFile
 
 def get_db():
     try:
@@ -7,5 +9,14 @@ def get_db():
         yield db
     finally:
         db.close()
+
+#saving img to backend
+def save_img(img: UploadFile = File(None)) -> str:
+    location: str = f"uploads/{img.filename}"
+    with open(location, "wb") as f:
+        shutil.copyfileobj(img.file, f)
+
+    return location
+
 
 oauth2_scheme = OAuth2PasswordBearer("/user/login")
